4baa3f1895d95f23b16c2c804245f8e9a4155d89
Oscar Coleman <161176613+oscarcoleman@users.noreply.github.com>
2025-11-22 19:07:37 +1100

    22/11

--- dice_game.sol
+++ dice_game.sol
@@ -1,23 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
 contract DiceGame {
     
-    uint public bankBalance;
-    
-    constructor() {
-        bankBalance = 0;
-    }
-    
-    function playGame(uint numToGamble, uint predictedDiceNum) public {
-        require(numToGamble > 0, "Gamble amount must be greater than zero");
-        
-    }
-    
-    function rollDice() private view returns (uint) {
-        return (uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % 6) + 1;
-    }
-    
-    function takeLoan(uint desiredLoanAmount) public {
-        bankBalance += desiredLoanAmount;
-    }
 }
