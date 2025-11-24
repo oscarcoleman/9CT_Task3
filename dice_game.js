10509bad0688155047447b54c9c526bfb5a3db7b
Oscar Coleman <161176613+oscarcoleman@users.noreply.github.com>
2025-11-24 13:20:54 +1100

    24/11

---
+++ dice_game.js
@@ -0,0 +1,18 @@
+let playButtonPressed = false;
+let loanButtonPressed = false;
+let helpButtonPressed = false;
+let inJail = false;
+let bankBalance = 100000;
+let jailBalance = 100000;
+
+function options() {
+    if (playButtonPressed === true) {
+        play();
+    } else if (loanButtonPressed === true) {
+        let loanAmount = prompt("Hey gambler, how much would you like to borrow?")
+    }
+}
+
+function play() {
+
+}
